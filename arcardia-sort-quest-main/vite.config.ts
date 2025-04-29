7494433a21a94ba7e61361580b591ba44ad93940
sahanabhogapur <2ke22cs122@kleit.ac.in>
2025-04-29 08:55:58 +0530

    Update base URL and rebuild

--- arcardia-sort-quest-main/vite.config.ts
+++ arcardia-sort-quest-main/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: '/arcardia-sort-quest/',
+  base: '/arhack/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
